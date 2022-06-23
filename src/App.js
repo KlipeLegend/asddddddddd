aa231bfae68f67681eea2b92a93a7d49b983bf94
KlipeLegend <suinbayevxumoyun20@gmail.com>
2022-06-23 09:49:23 +0500

    bellashuv

--- src/App.js
+++ src/App.js
@@ -1,114 +1,14 @@
 import './App.css';
-import Case from './pages/Case';
-import Dacode from './pages/Dacode';
-import All from './pages/All';
-import Blog from './pages/blog';
-import Service from './pages/Service';
-import Like from './pages/Like';
-  import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-// import  from './';
+import All from "./pages/All"
 function From() {
   return (
     <div className="From">
-      <Router>
-        <nav className="navbar ">
-
-          <ul className="navul">
-            <li>
-              <Link to="/" className="nav_link">Home</Link>
-            </li>
-            <li>
-              <Link to="/service" className="nav_link">Clothes</Link >
-
-            </li>
-            <li>
-              <Link to="/case" className="nav_link">Shoes</Link >
-
-            </li>
-
-
-
-
-          </ul>
-          <button className="Safari">
-            <img src="./img/icon/Safari.svg" alt="" />
-          </button>
-
-          <div className="logos">
-            <div className='logos1'>
-              <input type="text" placeholder='Search...' />
-              <img src="./img/icon/search.svg" alt="" />
-            </div>
-            <div className='logos2'>
-              <Link to="/person" className="nav_link"> <img src="./img/icon/person.svg" alt="" /> </Link >
-              <Link to="/shop" className="nav_link"> <img src="./img/icon/shop.svg" alt="" /></Link >
-              <Link to="/like" className="nav_link"> <img src="./img/icon/heart.svg" alt="" /> </Link >
-
-
-            </div>
-          </div>
-        </nav>
-
-        <Routes>
-          <Route path="/" exact element={<All />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/case" element={<Case />} />
-          <Route path="/person" element={<Dacode />} />
-          <Route path="/shop" element={<Blog />} />
-          <Route path="/like" element={<Like />} />
-        </Routes>
-      </Router>
-      <Footer />
-    </div>
+      <All/>
+        </div>
   );
 
 }
 
 export default From;
 
-export const Footer = () => {
-  return (
-    <div className="footer">
-      <div className="onsu">
-        <button class="ul">
-          <img src="./img/icon/Safari.svg" alt="" />
-        </button>
-        <div className="li">
-          <p>About us</p>
-          <p>Constact</p>
-          <p> Terms & Conditions</p>
-        </div>
-        <div className="li1">
-          <div>
-            <img src="./img/icon/facebook.svg" alt="" />
-            <p>Facebook</p>
-          </div>
-          <div>
-            <img src="./img/icon/twitter.svg" alt="" />
-            <p>Twitter</p>
-          </div>
-          <div>
-            <img src="./img/icon/instagram.svg" alt="" />
-            <p>Instagram</p>
-          </div>
-        </div>
-
-        <div className="li2">
-          <h1>Subscribe to our newslater</h1>
-          <form action="" className='form'>
-            <input type="text " placeholder='Email Addres' />
-            <button>OK</button>
-          </form>
-        </div>
-        <div className="li3">
-          <p>497 Evergreen Rd, Roseville, CA 95673</p>
-          <p>+44 345 678 903</p>
-          <p>xumoyun@gmail.com</p>
-        </div>
-      </div>
-
-
-    </div>
-  )
-}
+ 
--- src/pages/All.js
+++ src/pages/All.js
@@ -1,161 +1,19 @@
-import React, { useState } from "react";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import LocalGroceryStoreOutlinedIcon from '@mui/icons-material/LocalGroceryStoreOutlined';
-import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
-import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
-const Home = () => {
-  const[red,setRed] = useState(false);
-  const RedBtn = () =>{
-    setRed(!red)
-  }
-  const[shop,setShop] = useState(false);
-  const Shop = () =>{
-    setShop(!shop)
-  }
-  const [card, setCard] = useState([
-    {
-      id: 0,
-      img: './img/img/g1.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 1,
-      img: './img/img/g2.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 2,
-      img: './img/img/g1.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 3,
-      img: './img/img/g1.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 4,
-      img: './img/img/g1.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 5,
-      img: './img/img/g3.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 6,
-      img: './img/img/g4.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 7,
-      img: './img/img/g5.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 8,
-      img: './img/img/g6.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 9,
-      img: './img/img/g7.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 10,
-      img: './img/img/g2.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 11,
-      img: './img/img/g1.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    }, {
-      id: 12,
-      img: './img/img/9.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 13,
-      img: './img/img/g10.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 14,
-      img: './img/img/g1.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    },
-    {
-      id: 15,
-      img: './img/img/g11.png',
-      h1: 'Grey bee wrist-watch',
-      p: '$ 10.000'
-    }
-  ])
+import React from 'react'
+
+const Footer = () => {
   return (
-    <>
-      <div className="header">
-        <div className="opa">
-          <img src="./img/img/opa.png" alt="" />
-        </div>
-      </div>
-      <div className="diva">
-        <img src="./img/img/line.png" alt="" />
-        <h1 className="shop">Shop your style</h1>
-        <img src="./img/img/line.png" alt="" />
-      </div>
-
-      <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae gravida cursus adipiscing <br />viverra at tortor, egestas odio parturient. Morbi ut lorem in erat. Et et molestie diam diam ultricies. <br />Scelerisque duis diam ac cras dictum adipiscing. Venenatis at sit proin ut vitae adipiscing id facilisis.</p>
-      <div className="constainer">
-        {
-          card.map((val) => (
-            <div className="card" key={val.id}>
-              <div className="card_bg"></div>
-              <div className="card_pg"></div>
-              <div className="imag11">
-                <button onClick={RedBtn} className="favor">
-                  {
-                    red ?  <FavoriteOutlinedIcon/>  : <FavoriteBorderIcon />
-                  }
-                      
-                </button>
-              <button className="add_card" onClick={Shop}>
-              ADD TO CARD {shop ? <ShoppingCartRoundedIcon/> : <LocalGroceryStoreOutlinedIcon/>}
-              </button>
-              </div>
-              <div>
-                <img src={val.img} alt="" />
-                <div>
-                  <h1>{val.h1}</h1>
-                  <p>{val.p}</p>
-                </div>
-              </div>
+        <>
+         <div className="footer">
+            <div>
+                <img src="./img/fit" alt="" />
+                <p>© Copyright Finsweet 2022</p>
+                <h3>(480) 555-0103</h3>
+                <h3>4517 Washington Ave. </h3>
+                <h3>finsweet@example.com</h3>
             </div>
-          ))
-
-        }
-
-      </div>
-
-    </>
+         </div>
+        </>
   )
 }
-export default Home;
 
+export default Footer
